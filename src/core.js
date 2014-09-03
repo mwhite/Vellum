40701f919461bc3d7067cb3e86489cfdcb85902d
Daniel Miller <dmiller@dimagi.com>
2014-09-03 13:59:40 -0400

    Fix JavaRosa parse warning bug

--- src/core.js
+++ src/core.js
@@ -996,7 +996,10 @@ define([
                 _this.jstree("delete_node", el);
             });
             try {
+                // a place for plugins to put parse warnings
+                _this.data.core.parseWarnings = [];
                 _this.loadXML(formString);
+                delete _this.data.core.parseWarnings;
 
                 if (formString) {
                     _this._resetMessages(_this.data.core.form.errors);
@@ -1084,7 +1087,7 @@ define([
             mugTypes: this.data.core.mugTypes,
             allowedDataNodeReferences: this.opts().core.allowedDataNodeReferences, 
             externalInstances: this.opts().core.externalInstances
-        }, this);
+        }, this, _this.data.core.parseWarnings);
         form.formName = this.opts().core.formName || form.formName;
 
         form.on('question-type-change', function (e) {
